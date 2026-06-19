711894d72cbb06ded9f49453efaaeb848f94f743
Enmanuel <enmanuel@MacBook-Neo.local>
2026-06-19 19:08:34 -0400

    Product v0.4(Alhpa)

--- scripts/popup.js
+++ scripts/popup.js
@@ -2,6 +2,10 @@ export function popup(){
     const btnmenu = document.querySelector(".menu-toggle");
     const mobileMenu = document.getElementById("mobile-menu");
     const navBar= document.querySelector(".navbar")
+    const home= document.querySelector(".popup__option_home")
+    const contact= document.querySelector(".popup__option_contact")
+    const gallery=document.querySelector(".gallery__body")
+    console.log(gallery)
     if (!btnmenu || !mobileMenu) {
         console.error("Menú móvil o botón no encontrados en el DOM");
         return;
@@ -20,7 +24,20 @@ export function popup(){
             document.body.style.overflow = "hidden";
         }
     });
+    function comeBack(){
+ mobileMenu.classList.toggle("hidden");
+        navBar.className= "navbar";
+        document.body.style.overflow = "auto";
+    }
 
+contact.addEventListener("click", ()=>{
+      comeBack()
+        
+})
+home.addEventListener("click", ()=>{
+      comeBack()
+        
+})
     // Cerrar el menú si hacen clic fuera del contenedor (en el fondo oscuro)
     mobileMenu.addEventListener("click", (e) => {
         if (e.target === mobileMenu) {
